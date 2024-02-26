604e78a73e8d473d437083a59b762a04e6160282
victorHerediaa <161259744+victorHerediaa@users.noreply.github.com>
2024-02-26 11:17:53 -0800

    Delete DOM directory

--- DOM/script1/script1.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector('body')
- const colores = ['red', 'orange', 'brown','violet','pink' , 'blue', 'gray', 'yellow','green','turquoise']
-for(let i = 0 ; i<10 ; i++){
-    const div = document.createElement('div')
-    div.style.height = '100px'
-    div.style.backgroundColor = colores[Math.floor(Math.random()*colores.length)]
-    body.appendChild(div)
-} 
